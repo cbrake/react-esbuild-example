2188746637137efd81cfba1ca32ec3c5c12906d4
Cliff Brake <cbrake@bec-systems.com>
2022-03-21 23:12:32 -0400

    add height, which map needs

--- Map.jsx
+++ Map.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const position = [51.505, -0.09];
+const style = {height: "300px"};
 
 export const Map = () => {
   return (
-    <MapContainer center={position} zoom={13}>
+    <MapContainer center={position} zoom={13} style={style}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
